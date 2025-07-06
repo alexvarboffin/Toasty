d0f853185aa802b1d583df95063751fc00c78bd3
r.ivn <ishumakov881@gmail.com>
2025-07-06 20:17:01 +0300

    123

---
+++ app/src/main/java/com/atifqamar/customtoast/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.atifqamar.customtoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.dmoral.toastysample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
